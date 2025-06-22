e03e52121b2f904dab63920e3e51875a43a35690
labutnfrtl <erenovables.utn.frtl@gmail.com>
2025-06-22 00:43:46 -0300

    Acomode el orden de los import y from, agrege espacios entre las funciones

--- core/mainLoop.py
+++ core/mainLoop.py
@@ -1,14 +1,13 @@
 
+from datetime import datetime
 import time
 import core.carga_th as th
 import time
-from datetime import datetime
 import core.carga_th as th
 
 def mainLoop():
     count = 0
     minutos_validos = [0, 15, 30, 45]  # Cambialos si querés otros minutos
-
     print("Iniciando toma de datos cada 15 minutos...")
 
     while True:
@@ -21,7 +20,6 @@ def mainLoop():
             print(f"Lectura número: {count + 1}")
             count += 1
             time.sleep(60)  # Espera un minuto completo para evitar repetir
+
         else:
             time.sleep(1)  # Revisa cada segundo hasta que llegue el momento correcto
-
-
