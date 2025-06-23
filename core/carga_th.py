271c78a7514f8773c5b720c2ecca38f3c72f4518
labutnfrtl <erenovables.utn.frtl@gmail.com>
2025-06-23 15:12:38 -0300

    Agrege espacios entre lineas, y elimine tabulaciones.

--- core/carga_th.py
+++ core/carga_th.py
@@ -13,14 +13,14 @@ def cargar_datos_TH():
 array = dato.leer_datos()
 
     # Convertir los datos a float
-    sensor1_temp = float(array[0])
-    sensor1_hum  = float(array[1])
-    sensor2_temp = float(array[2])
-    sensor2_hum  = float(array[3])
-    sensor3_temp = float(array[4])
-    sensor3_hum  = float(array[5])
+sensor1_temp = float(array[0])
+sensor1_hum  = float(array[1])
+sensor2_temp = float(array[2])
+sensor2_hum  = float(array[3])
+sensor3_temp = float(array[4])
+sensor3_hum  = float(array[5])
 
     # Insertar datos por sensor (con idSensor 1, 2 y 3)
-    insertar_datos_TH(1, sensor1_temp, sensor1_hum)
-    insertar_datos_TH(2, sensor2_temp, sensor2_hum)
-    insertar_datos_TH(3, sensor3_temp, sensor3_hum)
+insertar_datos_TH(1, sensor1_temp, sensor1_hum)
+insertar_datos_TH(2, sensor2_temp, sensor2_hum)
+insertar_datos_TH(3, sensor3_temp, sensor3_hum)
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from database.constructor_db import inicializar
-import core.carga_th    as ds
 from core.mainLoop import mainLoop
+import core.carga_th    as ds
 
 if __name__ == '__main__':    
     inicializar()
     mainLoop()  # Inicia el bucle principal del programa  
     
-   
-
-
