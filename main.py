e6c0e5cf6c08f868e564dfc0fafb527834293c1b
labutnfrtl <erenovables.utn.frtl@gmail.com>
2025-06-23 21:30:10 -0300

    agregue control de excepciones

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import logging
 from database.constructor_db import inicializar
 from core.mainLoop import mainLoop
-import core.carga_th    as ds
+import core.carga_th as ds
 
-if __name__ == '__main__':    
-    inicializar()
-    mainLoop()  # Inicia el bucle principal del programa  
+logging.basicConfig(filename='app.log', level=logging.ERROR)
 
+if __name__ == '__main__':
+    try:
+        inicializar()
+    except Exception as e:
+        logging.error(f"Error al inicializar: {e}")
+
+    try:
+        mainLoop()
+    except Exception as e:
+        logging.error(f"Error en mainLoop: {e}")
